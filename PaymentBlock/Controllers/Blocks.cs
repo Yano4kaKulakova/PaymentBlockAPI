ff51eaaf797a7fa38f73cd2353e1f1f9be6bf00b
yano4 <ynakul@mail.ru>
2024-09-15 20:31:23 +0300

    Finally Final

---
+++ PaymentBlock/Controllers/Blocks.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentBlockAPI.Data;
+using PaymentBlockAPI.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace PaymentBlockAPI.Controllers
+{
+
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlocksController : Controller
+    {
+        private readonly PaymentBlockDbContext dbContext;
+
+        public BlocksController(PaymentBlockDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Получить список блокировок клиентов
+        /// </summary>
+
+        [HttpGet]
+        public async Task<IActionResult> GetClients()
+        {
+            return Ok(await dbContext.Blocks.ToListAsync());
+        }
+    }
+}
